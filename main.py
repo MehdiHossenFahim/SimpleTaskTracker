5b50fb857165cf8bdea89368f144863225d86d18
Mehedi Hossen Fahim <mehdihossenfahim100@gmail.com>
2026-04-22 00:24:14 +0600

    basic structure ready clean

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 #-------------------------Module 6 Assignment-------------------------
 #---------------------------------------------------------------------
 
-
 #---------------------------------------------------------------------
 #----------------------Title: Simple Task Tracker---------------------
 #---------------------------------------------------------------------
-
+import json
 class TaskManager:
     """a simple Python class where users can manage their daily tasks."""
     def  __init__(self):
-        self.tasks = []
+        try:
+            with open('tasks.json','r') as file:
+                self.tasks = json.load(file)
+        except FileNotFoundError:
+            self.tasks = []
 
     def add_task(self):
         """adds a task to the tasks list."""
@@ -18,6 +21,8 @@ class TaskManager:
         description = input("Enter Description: ")
         self.tasks.append({"title": title, "description": description})
         print("Task added successfully!")
+        with open(f"tasks.json","w") as file:
+            json.dump(self.tasks, file, indent=4)
 
     def view_tasks(self):
         """prints all the tasks in the tasks list."""
@@ -26,6 +31,7 @@ class TaskManager:
 
         if not self.tasks:
             print("Task list is empty.".center(24, " "))
+            return
 
         for idx,task in enumerate(self.tasks,start=1):
             print("\n"+f" Task {idx} ".center(24, "-"))
@@ -35,6 +41,7 @@ class TaskManager:
         """deletes a task from the tasks list."""
         if not self.tasks:
             print("\nTask list is empty!\nPlease add a task first.")
+            return
 
         else:
             self.view_tasks()
@@ -42,15 +49,21 @@ class TaskManager:
             try:
                 idx = int(input("\nEnter Task number to delete. (example: 1): "))
                 del self.tasks[idx-1]
+                self.save_tasks()
                 print(f"Task {idx} deleted successfully!")
             except ValueError:
                 print("Please enter a number.")
             except IndexError:
                 print("Task not found.")
 
+    def save_tasks(self):
+        """saves all the tasks in tasks.json."""
+        with open("tasks.json", "w") as file:
+            json.dump(self.tasks, file, indent=4)
 
 #-----------------------------Menu System-----------------------------
 def menu_options():
+    """Prints all the available options in the menu."""
     print("\n"+" Task Tracker ".center(24, "="))
     print("1. Add Task")
     print("2. View Tasks")
@@ -58,6 +71,7 @@ def menu_options():
     print("4. Exit")
 
 def menu_system(manager):
+    """Controls all the available options in the menu."""
     while True:
         menu_options()
         try:
